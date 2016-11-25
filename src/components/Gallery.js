57cf8e47647a4a0a35ad666e6999c3d060ad24a3
BT <bernardotolosajr@gmail.com>
2016-11-25 16:15:40 +0800

    rename tests folder

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,12 +4,6 @@ import { bindActionCreators } from 'redux'
 
 import  * as GalleryActions from '../actions.js';
 
-const flickrImages = [
-  "https://farm2.staticflickr.com/1581/25283151224_50f8da511e.jpg", "https://farm2.staticflickr.com/1653/25265109363_f204ea7b54.jpg",
-  "https://farm2.staticflickr.com/1571/25911417225_a74c8041b0.jpg",
-  "https://farm2.staticflickr.com/1450/25888412766_44745cbca3.jpg"
-];
-
 export class Gallery extends Component {
   componentDidMount() {
     this.props.loadImages();
